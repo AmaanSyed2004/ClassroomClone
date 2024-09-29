4be2b90a582b8be095ab36245380af08f67d0461
AmaanSyed2004 <amaansyed2004@gmail.com>
2024-09-29 20:14:31 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const connectToDB = require("./config/connectToDB");
+const allowCors = require("./middleware/cors");
+const cookieParser = require("cookie-parser");
+
+require("dotenv").config();
 const app = express();
 const port = 3000;
 connectToDB();
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use(express.json());
+app.use(allowCors);
+app.use(cookieParser());
+
+app.use("/", require("./routes/routes"));
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
 });
