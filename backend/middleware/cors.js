88936c2b4d7a64617bc78311d76ab2e6c452dea7
AmaanSyed2004 <amaansyed2004@gmail.com>
2024-09-29 19:27:53 +0530

    add cors middleware for frontend connection

---
+++ backend/middleware/cors.js
@@ -0,0 +1,13 @@
+const allowCors= (req,res,next)=>{
+    res.setHeader("Access-Control-Allow-Origin","http://localhost:5173");
+    res.setHeader("Access-Control-Allow-Methods","GET,POST,PUT,DELETE,PATCH,OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers","Content-Type,Authorization");
+    res.setHeader("Access-Control-Allow-Credentials",true);
+    if(req.method==="OPTIONS"){
+        return res.sendStatus(200);
+    }
+    else{
+        next();
+    }
+}
+module.exports=allowCors;
