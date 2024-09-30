eb051444ded58314d4530be2f39d0fcbcc4d5ed5
AmaanSyed2004 <amaansyed2004@gmail.com>
2024-09-30 07:02:55 +0530

    Create course schema

---
+++ backend/models/Course.js
@@ -0,0 +1,46 @@
+const mongoose= require('mongoose');
+const courseSchema= new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+    },
+    createdBy:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        required:true,
+    },
+    description:{
+        type:String,
+        required:true,
+    },
+    announcements:[{
+        type:String,
+    }],
+    students:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User',
+    }],
+    assignments:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'Assignment',
+    }],
+    createdAt:{
+        type:Date,
+        default:Date.now,
+    },
+    updatedAt:{
+        type:Date,
+        default: Date.now,
+    },
+    status:{
+        type:String,
+        enum:['active','archived'],
+        default:'active',
+    }
+});
+courseSchema.pre('save',function(next){
+    this.updatedAt= Date.now();
+    next();
+});
+const Course= mongoose.model('Course',courseSchema);
+module.exports=Course;
