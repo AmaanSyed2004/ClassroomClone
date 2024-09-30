81acd40587676f426d36b5365c4130a8a61d7a38
AmaanSyed2004 <amaansyed2004@gmail.com>
2024-09-30 06:54:04 +0530

    create role verification middleware

---
+++ backend/middleware/verifyStudent.js
@@ -0,0 +1,13 @@
+const jwt= require('jsonwebtoken');
+require('dotenv').config();
+const checkStudent=(req,res,next)=>{
+    const token= req.cookies.token;
+    if(!token) return res.status(401).json({Message:"Unauthorized"});
+    jwt.verify(token, process.env.ACCESS_SECRET, (err,user)=>{
+        if(err) return res.status(403).json({message:"Forbidden"});
+        if(!user.role==="student") return res.status(403).json({message:"Inavlid role, studnents cannot create a course."});
+        req.user=user;
+        return next();
+    })
+}
+module.exports=checkStudent;
