ce5ead94dc6fed2d91c510c8f3ed0dbd34f0d22a
AmaanSyed2004 <amaansyed2004@gmail.com>
2024-10-03 20:39:07 +0530

    feat: add mail invitation

---
+++ backend/utils/sendMail.js
@@ -0,0 +1,33 @@
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+const sendMail = async ({ email, courseID, title }) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "amaansbackup@gmail.com",
+        pass: process.env.APP_PASSWORD,
+      },
+    });
+    const mailOptions = {
+      from: "amaansbackup@gmail.com",
+      to: email,
+      subject: "Hey There! You are invited to join a classroom!",
+      text: `Hey there! You have been invited to join ${title} by ${name}. Please use this code: ${courseID} `,
+    };
+
+    // the function implicitly exectures the inner function to send the mail to the user as per the give email address
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        throw error; // explicitly throw an error which is being catched further down the fucntion call
+      }
+      console.log("Invitation has been sent");
+    });
+    
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+};
+
+module.exports = sendMail;
