9c45f2d6711038ecc8a17bdf1d083cdd60b9d120
AmaanSyed2004 <amaansyed2004@gmail.com>
2024-10-07 23:29:40 +0530

    add assignment schema

---
+++ backend/models/Assignments.js
@@ -0,0 +1,55 @@
+const mongoose=require('mongoose');
+const AssignmentsSchema= new mongoose.Schema({
+    courseID:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'Course',
+        required:true,
+    },
+    name:{
+        type:String,
+        required:true,
+    },
+    description:{
+        type:String,
+        required:true,
+    },
+    dueDate:{
+        type:Date,
+        required:true,
+    },
+    maxMarks:{
+        type:Number,
+        required:true,
+    },
+    submisions:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'Submission',
+    }],
+    createdAt:{
+        type:Date,
+        default:Date.now,
+    },
+    updatedAt:{
+        type:Date,
+        default: Date.now,
+    },
+    status:{
+        type:String,
+        enum:['active','archived'],
+        default:'active',
+    }
+
+});
+AssignmentsSchema.pre('save',function(next){
+    this.updatedAt= Date.now();
+    next();
+});
+//automatically close the assignment after due date
+AssignmentsSchema.pre('save',function(next){
+    if(this.dueDate<Date.now()){
+        this.status='archived';
+    }
+    next();
+});
+const Assignment= mongoose.model('Assignment',AssignmentsSchema);
+module.exports=Assignment;
