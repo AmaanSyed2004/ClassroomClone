89735ec7456757dbffdc7d43ac388a39f62b439b
AmaanSyed2004 <amaansyed2004@gmail.com>
2024-10-20 23:47:57 +0530

    add name to jwt

--- backend/utils/token.js
+++ backend/utils/token.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 const secret = process.env.ACCESS_SECRET;
 const generateToken = (user) => {
   return jwt.sign(
-    { id: user._id, email: user.email, role: user.role },
+    { id: user._id, email: user.email, role: user.role, name:user.name },
     secret,
     { expiresIn: "1h" }
   );
